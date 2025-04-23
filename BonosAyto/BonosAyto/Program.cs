88015071022d84bc7d0e1d0ce9f4e6b964ea4b72
Galsoft-Eloi-MI <eloi.m@galsoft.es>
2025-04-23 15:20:11 +0200

    Cambio a Program.cs

--- BonosAyto/BonosAyto/Program.cs
+++ BonosAyto/BonosAyto/Program.cs
@@ -1,4 +1,5 @@
 using BonosAyto.Components;
+using BonosAytoService;
 using BonosAytoService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddScoped<EstablecimientoService>();
 
 
 var app = builder.Build();
+ConexionBD.Inicilizar("Server=DESKTOP-B5B66KI\\SQLEXPRESS;Database=AytoCoruna;Trusted_Connection=True; TrustServerCertificate=True;");
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
