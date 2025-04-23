48c9e32bfe249a7f6dc0d61cfeeea14ce4181f6f
GalsoftPablo <pablo.c@galsoft.es>
2025-04-23 17:08:59 +0200

    Creación del archivo de Detalle de Usuarios

---
+++ BonosAyto/BonosAyto/Components/Pages/Usuarios/DetalleUsuarios.razor.cs
@@ -0,0 +1,33 @@
+﻿using BonosAytoService.DTOs;
+using BonosAytoService.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BonosAyto.Components.Pages.Usuarios
+{
+    public partial class DetalleUsuarios
+    {
+        [Inject]
+        private UsuarioService UsuarioService { get; set; }
+
+
+        [Parameter]
+        public int Id { get; set; }
+
+        private UsuarioDTO? usuario;
+
+        protected override void OnInitialized()
+        {
+            usuario = UsuarioService.Consultar(Id);
+        }
+
+        private void alumnosMatriculadosMaterias()
+        {
+            Navigate.NavigateTo($"/usuarios/detalle/{Id}/matriculas");
+        }
+
+        private void volverAListado()
+        {
+            Navigate.NavigateTo("/usuarios");
+        }
+    }
+}
