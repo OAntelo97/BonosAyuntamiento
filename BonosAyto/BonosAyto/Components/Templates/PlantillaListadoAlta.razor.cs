f3ddc2a485fb52ff30b8938cf418d81287ee83d5
Galsoft-Eloi-MI <eloi.m@galsoft.es>
2025-05-05 18:07:32 +0200

    Incorporación de plantilla a pantallas de listados
    La platilla está por terminar y aún faltan comprobaciones.

---
+++ BonosAyto/BonosAyto/Components/Templates/PlantillaListadoAlta.razor.cs
@@ -0,0 +1,86 @@
+﻿using BonosAyto.Components.Enums;
+using BonosAytoService.DTOs;
+using BonosAytoService.Model;
+using BonosAytoService.Services;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace BonosAyto.Components.Templates
+{
+    public partial class PlantillaListadoAlta
+    {
+        [Parameter]
+        public string? titulo { get; set; }
+        [Parameter]
+        public string? pestaña { get; set; }
+        [Parameter]
+        public string? alta { get; set; }
+        [Parameter]
+        public string? listado { get; set; }
+        [Parameter]
+        public RenderFragment? renderAlta { get; set; }
+        [Parameter]
+        public RenderFragment? renderListado { get; set; }
+        [Parameter]
+        public PageType tipo { get; set; }
+        /*
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            switch (tipo)
+            {
+                case PageType.Usuarios:
+                    titulo = "Gestión de Usuarios";
+                    pestaña = "Alta de Usuarios";
+                    alta = "Nuevo Usuario";
+                    listado = "Listado de Usuarios";
+                    break;
+                case PageType.Beneficiarios:
+                    titulo = "Gestión de Beneficiarios";
+                    pestaña = "Alta de Beneficiarios";
+                    alta = "Nuevo Beneficiario";
+                    listado = "Listado de Beneficiarios";
+                    break;
+                case PageType.Establecimientos:
+                    titulo = "Gestión de Establecimientos";
+                    pestaña = "Alta de Establecimientos";
+                    alta = "Nuevo Establecimiento";
+                    listado = "Listado de Establecimientos";
+                    break;
+            }
+        }
+        */
+        
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            switch (tipo)
+            {
+                case PageType.Usuarios:
+                    titulo = "Gestión de Usuarios";
+                    pestaña = "Alta de Usuarios";
+                    alta = "Nuevo Usuario";
+                    listado = "Listado de Usuarios";
+                    break;
+                case PageType.Beneficiarios:
+                    titulo = "Gestión de Beneficiarios";
+                    pestaña = "Alta de Beneficiarios";
+                    alta = "Nuevo Beneficiario";
+                    listado = "Listado de Beneficiarios";
+                    break;
+                case PageType.Establecimientos:
+                    titulo = "Gestión de Establecimientos";
+                    pestaña = "Alta de Establecimientos";
+                    alta = "Nuevo Establecimiento";
+                    listado = "Listado de Establecimientos";
+                    break;
+            }
+        }
+        
+    }
+}
